a3470f80c4a40fefce348e7a1d71a00f6e99c036
Serkan Atak <serkan_atak_2121@hotmail.com>
2025-05-11 23:06:29 +0300

    MAİN class bitti

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        String version = "1.0";
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMMM d, yyyy, HH:mm"));
+        System.out.println("FSM DESIGNER " + version + " " + timestamp);
 
+        Controller processor = new Controller();
+
+        if (args.length == 0) {
+            processor.starter();
+        } else if (args.length == 1) {
+            Executor.execute_File(args[0], processor);
+        } else {
+            System.out.println("Error: Too many arguments. Usage: java -jar fsm.jar [optional: filename]");
+        }
     }
 }
